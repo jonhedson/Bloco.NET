21df102341ba3d391234c6464232190cc9e0f3a0
Jonh Edson <kylix@hotmail.com>
2020-09-01 15:53:19 -0300

    DAW 2

---
+++ APIControllers/APIConsume/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIConsume.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace APIConsume.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Reservation> reservationList = new List<Reservation>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:8888/api/Reservation"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+
+        public ViewResult GetReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:8888/api/Reservation/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(reservation);
+        }
+
+        public ViewResult AddReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json"); 
+                
+                using (var response = await httpClient.PostAsync("http://localhost:8888/api/Reservation/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+
+        public async Task<IActionResult> UpdateReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:8888/api/Reservation/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(reservation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(reservation.Id.ToString()), "Id");
+                content.Add(new StringContent(reservation.Name), "Name");
+                content.Add(new StringContent(reservation.StartLocation), "StartLocation");
+                content.Add(new StringContent(reservation.EndLocation), "EndLocation");
+
+                using (var response = await httpClient.PutAsync("http://localhost:8888/api/Reservation", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Success";
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+        public async Task<IActionResult> DeleteReservation(int ReservationId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("http://localhost:8888/api/Reservation/" + ReservationId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
